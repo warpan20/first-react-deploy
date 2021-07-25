6536e714ab0b6a2c7a1bef81f4e1c606749e54b7
Arpan Sen <warpan2020@gmail.com>
2021-07-26 02:04:26 +0530

    second commit

---
+++ build/precache-manifest.38f0ff779569941581157c9857fdf8d5.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c657eb4ba82f169c785aee57d0135d9b",
+    "url": "/first-react-deploy/index.html"
+  },
+  {
+    "revision": "2ac5e7ffc73930e26b60",
+    "url": "/first-react-deploy/static/css/2.75c4ecca.chunk.css"
+  },
+  {
+    "revision": "4c629d13527c23c291a7",
+    "url": "/first-react-deploy/static/css/main.7a815947.chunk.css"
+  },
+  {
+    "revision": "2ac5e7ffc73930e26b60",
+    "url": "/first-react-deploy/static/js/2.670fdb07.chunk.js"
+  },
+  {
+    "revision": "361388382a7cba0b7cd58ae9b81faa0e",
+    "url": "/first-react-deploy/static/js/2.670fdb07.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c629d13527c23c291a7",
+    "url": "/first-react-deploy/static/js/main.c0aea3da.chunk.js"
+  },
+  {
+    "revision": "761fe33cf5a4dde956d9",
+    "url": "/first-react-deploy/static/js/runtime-main.d9bf0e27.js"
+  },
+  {
+    "revision": "19a2e0eab24e9e902408d4f8547eeab9",
+    "url": "/first-react-deploy/static/media/android.19a2e0ea.jpeg"
+  },
+  {
+    "revision": "70cea92af61399debee3717652a63569",
+    "url": "/first-react-deploy/static/media/app.70cea92a.jpeg"
+  },
+  {
+    "revision": "ac8f6f9ad22a8902b83e21560b95dca5",
+    "url": "/first-react-deploy/static/media/digital.ac8f6f9a.jpeg"
+  },
+  {
+    "revision": "619f0cc8062be2e032aa50e3e238b2a2",
+    "url": "/first-react-deploy/static/media/hero-img.619f0cc8.png"
+  },
+  {
+    "revision": "e981619f086d13be896e36768c26ce8f",
+    "url": "/first-react-deploy/static/media/img2.e981619f.svg"
+  },
+  {
+    "revision": "2eed95a2d555c404f1f451730eaa9eef",
+    "url": "/first-react-deploy/static/media/marketing.2eed95a2.jpg"
+  },
+  {
+    "revision": "1192a86d7075a15ba491f01cebceca0a",
+    "url": "/first-react-deploy/static/media/s1.1192a86d.jpg"
+  },
+  {
+    "revision": "43049cae060f2a2a2acb77007f8482f9",
+    "url": "/first-react-deploy/static/media/software.43049cae.jpeg"
+  }
+]);
--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -10,7 +10,7 @@ const Navbar = () => {
             <nav className="navbar navbar-expand-lg navbar-light bg-light">
               <div className="container-fluid">
                 <NavLink className="navbar-brand" to="/">
-                  ThapaTechnical
+                  SenSoftwares
                 </NavLink>
                 <button
                   className="navbar-toggler"
